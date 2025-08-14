ba85d66d14ae18c74ead2740f214fe1d1c2c97f2
e <e@e.com>
2025-08-14 18:31:34 -0500

    fixed block glitching

--- src/main/java/yuiopd/com/skyblockitems1214/Skyblockitems1214.java
+++ src/main/java/yuiopd/com/skyblockitems1214/Skyblockitems1214.java
@@ -1,19 +1,16 @@
 package yuiopd.com.skyblockitems1214;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
+
 import yuiopd.com.skyblockitems1214.a.ModItems;
-import yuiopd.com.skyblockitems1214.teleport.Aote;
+
 
 
 
 public class Skyblockitems1214 implements ModInitializer {
-    public static final Item aote = ModItems.register("aote", Aote::new, new Aote.Settings());
     @Override
     public void onInitialize() {
 
         ModItems.initialize();
-
-
     }
 }
